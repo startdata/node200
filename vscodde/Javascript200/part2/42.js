51751574d6fd88c810820551fa635cc8477a39af
startas <83121954+startdata@users.noreply.github.com>
2022-03-13 16:50:17 +0900

    42

---
+++ vscodde/Javascript200/part2/42.js
@@ -0,0 +1,17 @@
+// const URL = 'http://js.com';
+// URL = 'http://js.com';
+
+// if(true){
+//     const URL2 = 'http://js.com';
+// }
+
+// console.log(URL2);
+
+const CONST_USER = {name: 'jay', age: 30};
+console.log(CONST_USER.name,CONST_USER.age);
+
+CONST_USER.name = 'jay2';
+CONST_USER.age = 31;
+
+console.log(CONST_USER.name, CONST_USER.age);
+CONST_USER = {name: 'bbo'};

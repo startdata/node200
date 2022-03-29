efe594e95dccfc9fc3c169cc7b51c7100c6e24b0
startas <83121954+startdata@users.noreply.github.com>
2022-03-30 00:13:04 +0900

    73

---
+++ vscodde/Javascript200/part2/73.js
@@ -0,0 +1,7 @@
+
+const str1 = 'Good afternoon';
+const str2 = ', Good evening';
+const str3 = ', and Good night!';
+const str4 = ' - The Truman Show, 1998';
+console.log(str1.concat(str2,str3,str4));
+

10f90299312d90d4404b3a175098c07c3bfd47b5
startas <83121954+startdata@users.noreply.github.com>
2022-04-07 00:58:01 +0900

    94

---
+++ vscodde/Javascript200/part2/93.js
@@ -0,0 +1,8 @@
+const str = 'abcdefghijklmnopqrstuvwxyz';
+const arr = str.split('');
+arr.reverse();
+console.log(arr);
+arr.reverse();
+console.log(arr);
+console.log(arr.reverse());
+console.log(arr.join(''));

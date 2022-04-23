6f86c41d7138ab85300ee29fde8bab26c09fc28c
startas <tlwnsdlek@gmail.com>
2022-04-24 00:23:27 +0900

    123

---
+++ vscodde/Javascript200/part2/123.js
@@ -0,0 +1,23 @@
+const items = ['j', 'a', 'v', 'a', 's', 'c', 'r','i','p','t'];
+const seq = {
+    [Symbol.iterator](){
+        let i =0;
+        return {
+            next() {
+                const value = items[i];
+                i++;
+                const done = i > items.length;
+                return {value, done}
+            }
+        }
+    }
+};
+console.log(seq);
+for (let s of seq) console.log(s);
+console.log(seq);
+const [a,b,c, ...arr] = seq;
+console.log(arr);
+console.log('a >>>', a);
+console.log('b >>>', b);
+console.log('c >>>', c);
+console.log('arr >>>', arr);

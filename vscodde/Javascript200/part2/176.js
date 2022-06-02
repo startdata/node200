e01b8682b3f5515991bfc6e494a24ae69fd23717
startas <tlwnsdlek@gmail.com>
2022-06-03 02:32:38 +0900

    176

---
+++ vscodde/Javascript200/part2/176.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const http = require('http');
+const url = require('url');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res) => {
+    switch (req.method) {
+        case 'GET':
+            if (req.url === '/') {
+                res.setHeader('Content-Type', 'text/plain')
+                res.writeHead(200);
+                res.end('Hello');
+            } else if(req.url.substring(0,5) === '/data') {
+                const queryParams = url.parse(req.url, ture).query;
+
+                res.setHeader('Content-Type', 'text/html');
+                res.writeHead(200);
+                res.write(`<html><head><title>javascript 200</title></head>`);
+
+                for(let key in queryParams){
+                    res.write(`<h1>${key}</h1>`);
+                    res.write(`<h2>${queryParams[key]}</h2>`);
+                }
+                
+                res.end(`</body></html>`);
+            }
+            break;
+            default:
+                res.end();
+    }
+
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});

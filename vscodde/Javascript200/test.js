bf36e1049d47468db833f9957e9d5122a9b2f239
startas <tlwnsdlek@gmail.com>
2022-06-09 23:44:14 +0900

    test

--- vscodde/Javascript200/test.js
+++ vscodde/Javascript200/test.js
@@ -1,5 +1,11 @@
-for(let i =1; i<10; i++){
-    for(let j=1; j<10; j++){
-        console.log(i + '단' + '=' +i+'*'+j + '=' +i*j);
-    }
+let a = 10;
+let b = 60;
+
+const foo = (a) => {
+    a = 100;
+
+    a+b;
 }
+
+console.log(foo(a));
+console.log(b + a++);

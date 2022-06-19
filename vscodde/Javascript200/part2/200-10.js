d79066f0101c976c9662f4f7bbf3193f35930805
startas <tlwnsdlek@gmail.com>
2022-06-19 20:43:51 +0900

    200-10

---
+++ vscodde/Javascript200/part2/200-10.js
@@ -0,0 +1,19 @@
+const floor1 = Math.floor(45.95);
+const floor2 = Math.floor(4);
+const floor3 = Math.floor(NaN);
+const floor4 = Math.floor(-45.05);
+
+const ceil1 = Math.ceil(7.004);
+const ceil2 = Math.ceil(-0.95);
+const ceil3 = Math.ceil(-4);
+const ceil4 = Math.ceil(-7.004);
+
+console.log('floor1:', floor1);
+console.log('floor2:', floor2);
+console.log('floor3:', floor3);
+console.log('floor4:', floor4);
+
+console.log('ceil1:', ceil1);
+console.log('ceil2:', ceil2);
+console.log('ceil3:', ceil3);
+console.log('ceil4:', ceil4);
---
+++ vscodde/Javascript200/part2/200-9.js
@@ -0,0 +1,20 @@
+const log_1 = Math.log(10);
+const log_2 = Math.log(1);
+
+const log10_1 = Math.log10(10000);
+const log10_2 = Math.log10(10);
+
+const log2_1 = Math.log2(1024);
+const log2_2 = Math.log2(2);
+
+const log1p_1 = Math.log1p(1);
+const log1p_2 = Math.log1p(0);
+
+console.log('log_1:', log_1);
+console.log('log_2:', log_2);
+console.log('log10_1:', log10_1);
+console.log('log10_2:', log10_2);
+console.log('log2_1:', log2_1);
+console.log('log2_2:', log2_2);
+console.log('log1p_1:', log1p_1);
+console.log('log1p_2:', log1p_2);

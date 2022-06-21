8ac97e11885051e7f889ad0249c1387a768e2d31
startas <tlwnsdlek@gmail.com>
2022-06-21 22:28:06 +0900

    200-12

---
+++ vscodde/Javascript200/part2/200-12.js
@@ -0,0 +1,15 @@
+const exp1 = Math.exp(-1);
+const exp2 = Math.exp(0);
+const exp3 = Math.exp(1);
+
+const expm1_1 = Math.expm1(-1);
+const expm1_2 = Math.expm1(0);
+const expm1_3 = Math.expm1(1);
+
+
+console.log('exp1:' , exp1);
+console.log('exp2:' , exp2);
+console.log('exp3:' , exp3);
+console.log('expm1_1:' , exp1_1);
+console.log('expm1_2:' , exp1_2);
+console.log('expm1_3:' , exp1_3);

4501f85eaf8fd4d0b916599e806c0e1c26bf102e
startas <tlwnsdlek@gmail.com>
2022-06-22 00:30:31 +0900

    200-13

---
+++ vscodde/Javascript200/part2/200-13.js
@@ -0,0 +1,18 @@
+const sin1 = Math.sin(0);
+const sin2 = Math.sin(1);
+const sin3 = Math.sin(Math.PI/2);
+
+const cos1 = Math.cos(0);
+const cos2 = Math.cos(1);
+const cos3 = Math.cos(Math.PI/4);
+
+const tan1 = Math.tan(Math.PI/4);
+
+
+console.log('sin1', sin1);
+console.log('sin2', sin2);
+console.log('sin3', sin3);
+console.log('cos1', cos1);
+console.log('cos2', cos2);
+console.log('cos3', cos3);
+console.log('tan', tan1);

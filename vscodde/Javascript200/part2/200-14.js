c6dcf96111105e79fcd16328b6cac9b73f64a7ad
startas <tlwnsdlek@gmail.com>
2022-06-23 00:25:04 +0900

    200-14

---
+++ vscodde/Javascript200/part2/200-14.js
@@ -0,0 +1,17 @@
+const asin1 = Math.asin(0);
+const asin2 = Math.asin(1);
+const asin3 = Math.asin(Math.PI /2);
+
+const acos1 = Math.acos(0);
+const acos2 = Math.acos(1);
+const acos3 = Math.acos(-1.2);
+
+const atan1 = Math.atan(Math.PI/2);
+
+console.log('asin1:', asin1);
+console.log('asin2:', asin2);
+console.log('asin3:', asin3);
+console.log('acos1:', acos1);
+console.log('acos2:', acos2);
+console.log('acos3:', acos3);
+console.log('atan1:', atan1);

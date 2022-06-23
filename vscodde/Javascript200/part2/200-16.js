8ae2dedb1c4075539725c473fc248f4e2ad8dba3
startas <tlwnsdlek@gmail.com>
2022-06-23 23:14:52 +0900

    200-16

---
+++ vscodde/Javascript200/part2/200-16.js
@@ -0,0 +1,21 @@
+const asinh1 = Math.asinh(1);
+const asinh2 = Math.asinh(0);
+const asinh3 = Math.asinh(-1);
+
+const acosh1 = Math.acosh(-1);
+const acosh2 = Math.acosh(0);
+const acosh3 = Math.acosh(1);
+
+const atanh1 = Math.atanh(-1);
+const atanh2 = Math.atanh(0);
+const atanh3 = Math.atanh(2);
+
+console.log('asinh1:', asinh1);
+console.log('asinh2:', asinh2);
+console.log('asinh3:', asinh3);
+console.log('acosh1:', acosh1);
+console.log('acosh2:', acosh2);
+console.log('acosh3:', acosh3);
+console.log('atanh1:', atanh1);
+console.log('atanh2:', atanh2);
+console.log('atanh3:', atanh3);

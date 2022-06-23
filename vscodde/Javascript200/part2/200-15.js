5a8ebd46ade5a5b381a98e25fa21da5808b3aae5
startas <tlwnsdlek@gmail.com>
2022-06-23 23:11:12 +0900

    200-15

---
+++ vscodde/Javascript200/part2/200-15.js
@@ -0,0 +1,19 @@
+const sinh1 = Math.sinh(0);
+const sinh2 = Math.sinh(1);
+const sinh3 = Math.sinh(-1);
+
+const cosh1 = Math.cosh(0);
+const cosh2 = Math.cosh(1);
+const cosh3 = Math.cosh(-1);
+
+const tanh1 = Math.tanh(0);
+const tanh2 = Math.tanh(Infinity);
+
+console.log('sinh1:', sinh1);
+console.log('sinh2:', sinh2);
+console.log('sinh3:', sinh3);
+console.log('cosh1:', sinh1);
+console.log('cosh2:', sinh2);
+console.log('cosh3:', sinh3);
+console.log('tanh1:', tanh1);
+console.log('tanh2:', tanh2);

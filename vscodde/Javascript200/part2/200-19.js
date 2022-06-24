48db9946827b55eb03ba518f3c287e91c26cd8af
startas <tlwnsdlek@gmail.com>
2022-06-24 22:20:27 +0900

    200-19

---
+++ vscodde/Javascript200/part2/200-19.js
@@ -0,0 +1,9 @@
+const string = 'abcdefghijklmnopqrstuvwxyz';
+
+const subString1 = string.substring(0,4);
+const subString2 = string.substring(8,11);
+const subString3 = string.substring(24,25);
+
+console.log('0,4', subString1);
+console.log('8,11', subString2);
+console.log('24,25', subString3);

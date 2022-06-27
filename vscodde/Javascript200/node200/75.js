cd5d100fe4ec24804e593c649b9af6f3f869ce6c
startas <tlwnsdlek@gmail.com>
2022-06-27 17:40:11 +0900

    78

---
+++ vscodde/Javascript200/node200/75.js
@@ -0,0 +1,8 @@
+const string = '<h1> 특별가 - </h1><h2>99,000</h2>';
+const result1 = string.replace(/<..>/g, '');
+const result2 = string.replace(/<.../g, '');
+
+console.log('<..> :', result1);
+console.log('<... :', result2);
+
+
--- vscodde/Javascript200/node200/76.js
+++ vscodde/Javascript200/node200/76.js
@@ -1,6 +1,7 @@
-const string = '<h1> 특별가 - </h1><h2>99,000</h2>';
-const result1 = string.replace(/<..>/g, '');
-const result2 = string.replace(/<.../g, '');
+const string = '<h1>특별가 - </h1><h2>';
+const stringDiv = '<div>특별가 - </div><h2>99,000원</h2>';
+const replaceH1 = string.replace(/<.{0,1}h1>/g, '');
+const replaceTags = stringDiv.replace(/<.{0,4}>/g, '');
 
-console.log('<..> :', result1);
-console.log('<... :', result2);
+console.log(replaceH1);
+console.log(replaceTags);

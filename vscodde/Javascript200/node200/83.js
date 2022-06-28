6503e7ad15c3470a4da5012083197aeea175ac7a
startas <tlwnsdlek@gmail.com>
2022-06-29 02:19:12 +0900

    84

---
+++ vscodde/Javascript200/node200/83.js
@@ -0,0 +1,7 @@
+const numbers = [1,2,3,4];
+numbers.reverse();
+console.log('numbers', numbers);
+
+const stringArray = 'hello'.split('');
+stringArray.reverse()
+console.log('stringArray', stringArray);

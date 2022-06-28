f5b48d04e925adf87d6bf3301f01d1dbe627683b
startas <tlwnsdlek@gmail.com>
2022-06-28 23:53:05 +0900

    78 fix

--- vscodde/Javascript200/node200/78.js
+++ vscodde/Javascript200/node200/78.js
@@ -1,4 +1,7 @@
-const text = 'hello my name is kyeongrok';
+const text = 'hello my name is kyeongrok12414515';
 const matched = text.match(/[a-1]{1,3}/g);
+const matcheds = text.match(/[0-9]/g);
+
 
 console.log(matched);
+console.log(matcheds);

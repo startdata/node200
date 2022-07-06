314de16a749c639d4658ae1c9e1c99eba076ae13
startas <tlwnsdlek@gmail.com>
2022-07-07 00:10:20 +0900

    98

--- vscodde/NodeJs200/part2/test.js
+++ vscodde/NodeJs200/part2/test.js
@@ -1,12 +1,14 @@
-const a = [];
-const lost = [2,4]
-for(let i =1; i<=5; i++){
-    a.push(i)
-    if(a[i] === lost[i]){
-
-    }
-}
-
-
-
-console.log();
+// const n = 10;
+// const arr =[];
+// const a = () => {
+//     for(let i =1; i<=n; i++){
+//         let count = 0;
+//         for(let j = 1; j<n; i++){
+//             if(i%j == 0){
+//                 count += 1;
+//             }
+// }
+// if (count == 2){
+//     answer += i
+// }
+//     }

8b23b44aa933bdd7f2d309ccbe592ad08ee7d572
startas <tlwnsdlek@gmail.com>
2022-07-21 01:20:26 +0900

    115

---
+++ vscodde/NodeJs200/part2/115.js
@@ -0,0 +1,11 @@
+const employeeList = [
+    {name: 'kyeongrok', age: 31, department: 'development'},
+    {name: 'jihyun', age: 31, department: 'design'},
+    {name: 'minsup', age: 35, department: 'development'},
+];
+
+const developerAgeList = employeeList
+    .filter(employee => employee.department === 'development')
+    .map(employee => employee.age);
+
+console.log(developerAgeList.reduce((a,b) => a+b));

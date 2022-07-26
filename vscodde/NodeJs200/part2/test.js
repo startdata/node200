eab5b82feb9a78fdeaed2f3242b777e22d5d3dc9
startas <tlwnsdlek@gmail.com>
2022-07-27 00:21:54 +0900

    124

--- vscodde/NodeJs200/part2/test.js
+++ vscodde/NodeJs200/part2/test.js
@@ -1,18 +1,7 @@
-// const strings = ["sun", "bed", "car"]
+const s = "abcde"
+var answer = '';
+let char = s.split('')
+let middle = Number(char.length / 2)
+answer == char[middle] 
 
-// function solution(strings, n){
-//     var answer = [];
-//     for(let i =0; i<strings.length; i++){
-//         let str = strings[i].split('')[n]
-//         answer.push(str)
-//     }
-//     answer.sort()
-// }
-// console.log(solution(strings,1));
-const cc = [{a:2,b:3,c:5},{a:2,b:5,c:5},{a:2,b:5,c:5},{a:1,b:2,c:5},{a:1,b:3,c:5}]
-
-const bb = cc.filter(list => {
-    list.a > 2
-})
-
-console.log(bb);
+console.log(answer);

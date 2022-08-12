e762a6331bb889aa5dfe2782ce82f0637f9838ec
startas <tlwnsdlek@gmail.com>
2022-08-13 02:17:29 +0900

    141

---
+++ vscodde/NodeJs200/part2/141.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+http.createServer((req,res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Set-Cookie' : ['soju = grilledPork', 'beer = chicken']
+    });
+
+
+    res.end(`<h1>${req.headers.cookie}</h1>`)
+}).listen(50000, () => {
+    console.log('서버가 실행중');
+})

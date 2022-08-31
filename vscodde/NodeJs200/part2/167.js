ff95cbfcf5d70ef0b8c09b4c3a03d3764276300c
startas <tlwnsdlek@gmail.com>
2022-09-01 00:32:01 +0900

    167

---
+++ vscodde/NodeJs200/part2/167.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("combined"));
+app.use(morgan("common"));
+app.use(morgan(":method + :date"));
+app.use(morgan(":status + :url"));
+app.use((req, res) => {
+  res.send("express morgan");
+});
+
+app.listen(3000, () => {
+  console.log(`server run`);
+});

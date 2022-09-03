433932ca3142080946e4b863948058bb713609f5
startas <tlwnsdlek@gmail.com>
2022-09-03 19:27:53 +0900

    169: ;

---
+++ vscodde/NodeJs200/part2/169.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+const mutipart = require("connect-multiparty");
+
+const app = express();
+
+app.use(multipart({ uploadDir: `${__dirname}/upload` }));
+
+app.get("/", (req, res) => {
+  fs.readFile("connect-multiparty.html", (error, data) => {
+    res.send(data.toString());
+  });
+});
+
+app.post("/", (req, res) => {
+  const imgFile = req.files.image;
+  const outputPath = `${__dirname}/upload/${Date.now()}_${imgFile.name}`;
+  console.log(outputPath);
+  console.log(req.body, req.files);
+  fs.rename(imgFile.path, outputPath, () => {
+    res.redirect("/");
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`server run`);
+});

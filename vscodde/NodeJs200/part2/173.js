4ad64177a2e7d820b3a9c42588dcaab86a8be977
startas <tlwnsdlek@gmail.com>
2022-09-07 00:15:10 +0900

    173

---
+++ vscodde/NodeJs200/part2/173.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "gmailId@gmail.com",
+    pass: "password",
+  },
+});
+
+const mailOptions = {
+  from: "gmailFrom@gmail.com", // send address
+  to: "gmailTo@gmail.com", // list of receivers
+  subject: "Hello ", // Subject line
+  text: `<h1>Hello world??</h1>`,
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`Message sent: ${info.response}`);
+  }
+  transporter.close();
+});

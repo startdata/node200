b8aa7d11d703625049ebe6e5101f97726cba682a
startas <tlwnsdlek@gmail.com>
2022-10-09 12:51:07 +0900

    102-1

---
+++ vscodde/NodeJs200/part2/102-1.js
@@ -0,0 +1,9 @@
+const multiple5 = (x) => x + 5;
+const add10 = (x) => x + 10;
+
+const plus = (a, b) => a + b;
+const minus = (a, b) => a - b;
+
+console.log(multiple5(add10(20)));
+
+console.log(minus(plus(10, 20), 40));
